e6f6c6c059b3eb7423f047ba28d2103496acc93a
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-10-16 00:32:04 +0530

    packages > ui: Prisma ORM init

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+  return (
+    <div className="text-4xl self-center">
+      <Button appName="user-app" >Helllo</Button>
+    </div>
+  );
+}
