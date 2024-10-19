8b1b018b3875b8426fc04615a3406a29d2a6947c
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-10-19 23:29:30 +0530

    user > lib > auth: next-auth provider setup

--- apps/user-app/lib/auth.ts
+++ apps/user-app/lib/auth.ts
@@ -1,4 +1,4 @@
-import client from '@repo/db/client'
+import prisma from '@repo/payTN-db/client'
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from 'bcrypt'
 
@@ -11,10 +11,58 @@ export const authProviders = {
                 password: { label: "Password", type: "password", placeholder: "Enter the password" }
             },
             async authorize(credentials: any) {
-                const hashedPass = bcrypt.hash(credentials.password, 10)
+                const hashedPass = await bcrypt.hash(credentials.password, 10)
 
-                const existingUser = await client.user.findUniwuq
+                const existingUser = await prisma.user.findFirst({
+                    where: {
+                        number: credentials.phone
+                    },
+                })
+
+                if (existingUser) {
+                    const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password)
+                    if (passwordValidation) {
+                        return {
+                            id: existingUser.id.toString(),
+                            name: existingUser.name,
+                            email: existingUser.email
+                        }
+                    }
+                    return {
+                        status: 500,
+                        message: "Password doesn't match"
+                    }
+                }
+
+                try {
+                    const newUser = await prisma.user.create({
+                        data: {
+                            number: credentials.phone,
+                            password: hashedPass
+                        }
+                    })
+
+                    if (newUser) {
+                        return {
+                            id: newUser.id.toString(),
+                            email: newUser.email,
+                            name: newUser.name
+                        }
+                    }
+                } catch (error) {
+                    console.log(error)
+                }
+
+                return null;
             }
         })
-    ]
+    ],
+    secret: process.env.JWT_SECRET || "secret",
+    callbacks: {
+        async sesion({ token, session }: any) {
+            session.user.id = token.sub
+
+            return session
+        }
+    }
 }
