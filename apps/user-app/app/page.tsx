47657422f1e3ff80e4d4208130365dc7b521c73d
vinodkumarmurmu62 <vinodkumarmurmu62@gmail.com>
2024-10-22 10:01:37 +0000

    app > page: appbar init

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,12 @@
 import {Button} from "@repo/ui/components/ui/button"
+import Appbar from "@repo/ui/components/custom/Appbar"
 
 export default function Home() {
   return (
-    <div className="text-4xl self-center">
-      <Button >Helllo from cloud</Button>
+    <div className="text-2xl self-center">
+      <div className="px-5 py-2 border-b-2">
+        <Appbar/>
+      </div>
     </div>
   );
 }
